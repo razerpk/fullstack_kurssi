b823fe2919188132fd12b0dd7c8f26251361c8af
Rico Pietarinen <rico.pietarinen@gmail.com>
2019-07-03 10:24:24 +0300

    deleting rest of the obsolete code and files

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import './App.css';
 import Country from './components/Country'
 import countriesServices from './services/countries'
 import axios from 'axios'
